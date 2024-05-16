5c0711fd588e90332635994976cdf00f45ed2982
Emir Akinci <98558882+emirakinci@users.noreply.github.com>
2024-05-16 15:28:48 +0200

    Add files via upload

---
+++ java_exercises/Arrays2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Arrays2 {
+
+    public static void printElements(int[] array)
+    {
+        int index = 0;
+        for(int element:array)
+        {
+            System.out.println("array[" + index +"] = " + element);
+            index++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = new int[5];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type your numbers:");
+
+        for(int i=0; i<5; i++)
+        {
+            a[i] = scanner.nextInt();
+        }
+
+        System.out.println("****************");
+        printElements(a);
+
+        scanner.close();
+    }
+}
